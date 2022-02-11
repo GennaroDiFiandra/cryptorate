59ec9446ea0881e369bc769bf243a37cc48145de
Gennaro Di Fiandra <GennaroDiFiandra@users.noreply.github.com>
2022-02-11 14:43:31 +0100

    Implement some minor code refactoring

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -2,6 +2,7 @@ import './CryptoList.css';
 import Crypto from './Crypto';
 
 function CryptoList({ cryptoList }) {
+  const FinalCryptoList = cryptoList.filter(crypto => crypto.type === 'crypto' && crypto.name !== 'Bitcoin').map(crypto => <Crypto key={crypto.unit} name={crypto.name} value={crypto.value} />);
   return (
     <div className='CryptoList__wrapper'>
       <table className='CryptoList'>
@@ -12,11 +13,7 @@ function CryptoList({ cryptoList }) {
           </tr>
         </thead>
         <tbody>
-          {
-            cryptoList
-              .filter(crypto => crypto.type === 'crypto' && crypto.name !== 'Bitcoin')
-              .map(crypto => <Crypto key={crypto.unit} name={crypto.name} value={crypto.value} />)
-          }
+          {FinalCryptoList}
         </tbody>
       </table>
     </div>
