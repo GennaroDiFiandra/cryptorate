d0a802ce754b7742340fbf344ad3a7ee0a6ca725
Gennaro Di Fiandra <GennaroDiFiandra@users.noreply.github.com>
2022-02-14 10:06:32 +0100

    Add the focus to the input filter on the app first render

--- src/components/CryptoFilter.js
+++ src/components/CryptoFilter.js
@@ -1,9 +1,17 @@
+import { useEffect, useRef } from 'react';
 import './CryptoFilter.css';
 
 function CryptoFilter({ onCryptoFilterChange }) {
+
+  const inputFilter = useRef();
+
+  useEffect(() => {
+    inputFilter.current.focus();
+  }, []);
+
   return (
     <div className="CryptoFilter">
-      <input className="CryptoFilter__input" type="search" placeholder="filter by crypto name" onChange={onCryptoFilterChange} />
+      <input className="CryptoFilter__input" type="search" placeholder="filter by crypto name" onChange={onCryptoFilterChange} ref={inputFilter} />
     </div>
   );
 }
